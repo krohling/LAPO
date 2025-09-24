8e1462f13e1f382477676ecd4da1d6e660b903e1
Kevin Rohling <krohling@cs.utexas.edu>
2025-09-24 15:37:42 -0500

    fix minor bug in config variables after merge with dev branch

--- lapo/config.py
+++ lapo/config.py
@@ -89,8 +89,8 @@ class Stage1Config:
     n_eval_steps: int
     n_valid_eval_sample_images: int
     n_test_eval_sample_images: int
-    load_checkpoint: str | None = None
     only_eval_test: bool
+    load_checkpoint: str | None = None
     image_loss: ImageLossConfig = field(default_factory=ImageLossConfig)
 
 
